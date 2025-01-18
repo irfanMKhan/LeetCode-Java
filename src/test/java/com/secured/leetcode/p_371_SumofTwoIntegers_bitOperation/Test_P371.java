c10100c390cb739a415c3c3d365084f103be52ae
irfanMKhan <irfanupdate@yahoo.com>
2025-01-18 18:23:29 -0500

    Solve problem 371

---
+++ src/test/java/com/secured/leetcode/p_371_SumofTwoIntegers_bitOperation/Test_P371.java
@@ -0,0 +1,11 @@
+package com.secured.leetcode.p_371_SumofTwoIntegers_bitOperation;
+
+import org.junit.jupiter.api.Test;
+
+class Test_P371 {
+    @Test
+    void test_BitOperation() {
+        System.out.println(P_371.getSum(5, 7));
+    }
+
+}
