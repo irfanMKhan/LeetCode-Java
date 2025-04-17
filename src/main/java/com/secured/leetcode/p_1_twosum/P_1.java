f687be2a9cf23282c6dc6f0c87b59c6bb81982d4
irfanMKhan <irfanupdate@yahoo.com>
2025-04-17 14:07:51 -0500

    Update

--- src/main/java/com/secured/leetcode/p_1_twosum/P_1.java
+++ src/main/java/com/secured/leetcode/p_1_twosum/P_1.java
@@ -1,11 +1,12 @@
 package com.secured.leetcode.p_1_twosum;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class P_1 {
 
     public static int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(target - nums[i])) {
                 return new int[]{map.get(target - nums[i]), i};
@@ -17,13 +18,12 @@ public class P_1 {
 
     public static int[] twoSum_withoutHashmap(int[] nums, int target) {
 
-        for (int i = 1; i < nums.length; i++) {
-            for (int j = i; j < nums.length; j++) {
-                if (nums[j] + nums[j - i] == target) {
+        for (int i = 1; nums.length > i; i++) {
+            for (int j = i; nums.length > j; j++) {
+                if (target == nums[j - i] + nums[j])
                     return new int[]{j - i, j};
-                }
             }
         }
-        return new int[0];
+        return new int[]{};
     }
 }
