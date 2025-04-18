2eba0c9e6d603f57f2cdfa7c092dc8d233689614
irfanMKhan <irfanupdate@yahoo.com>
2025-04-18 11:32:50 -0500

    Update

--- src/test/java/com/secured/InterviewQuestions/TestSeatingArrangements.java
+++ src/test/java/com/secured/InterviewQuestions/TestSeatingArrangements.java
@@ -1,6 +1,6 @@
 package com.secured.InterviewQuestions;
 
-import com.secured.InterviewQuestions.SeatingArrangements;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestSeatingArrangements {
@@ -8,6 +8,7 @@ class TestSeatingArrangements {
     @Test
     void test() {
         System.out.println(SeatingArrangements.arrange(new int[]{5, 6, 10, 8}));
+        Assertions.assertEquals(4, SeatingArrangements.arrange(new int[]{5, 6, 10, 8}));
     }
 
 }
--- src/test/java/com/secured/leetcode/p_20_ValidParentheses/Test_P20.java
+++ src/test/java/com/secured/leetcode/p_20_ValidParentheses/Test_P20.java
@@ -1,13 +1,14 @@
 package com.secured.leetcode.p_20_ValidParentheses;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class Test_P20 {
 
     @Test
     public void solve() {
-        String input = "()[]{{()}}}";
-        System.out.println(P_20.isValid(input));
+        Assertions.assertFalse(P_20.isValid("{}["));
+        Assertions.assertTrue(P_20.isValid("()[]{{()}}[]"));
     }
 
 }
